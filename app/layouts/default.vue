4c89873b93edff484f5eb19d093df54147480f3d
Don <don.puerto@hotmail.com>
2025-04-23 06:35:40 +0800

    ..

---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLoadingIndicator :color="false" class="z-100 bg-primary/80" />
+  <header class="bg-background sticky inset-x-0 top-0 isolate z-10 flex shrink-0 items-center gap-2 border-b">
+    
+    <div class="flex h-14 w-full items-center gap-2 px-4">
+
+      <slot />
+    </div>
+  </header>
+ 
+</template>
+
