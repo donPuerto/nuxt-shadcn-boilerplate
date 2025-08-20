51ec7b8ba367fd4aade06917376d9052f4209de5
Don <don.puerto@hotmail.com>
2025-08-20 10:52:16 +0800

    ..

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,6 +3,7 @@
     <div>
     <NuxtLink to="/">Home</NuxtLink> <br>
     <NuxtLink to="/docs/getting-started">Documentation</NuxtLink>
+    <Button>Click me</Button>
   </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,6 @@
+import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools:{ enabled: true },
@@ -11,6 +14,29 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxt/test-utils',
+    'shadcn-nuxt',
   ],
+  css: ['@/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tsconfigPaths(),
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './app/components/ui'
+  },
+
+  content: {
+    experimental: { nativeSqlite: true },
+  },
   
 })
